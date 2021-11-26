bda75fda9f098a35c48d9a4da5141f4eb4fb4d87
Nikita Semenov <kondrieger@gmail.com>
2021-11-26 19:51:49 +0300

    photo view done

--- views/Photos/Photos.vue
+++ views/Photos/Photos.vue
@@ -4,23 +4,22 @@
             <h2 data-aos="fade-right" data-aos-once="true">Галерея</h2>
             <div class="photos__wrap" data-aos="fade-up" data-aos-once="true">
                 <div class="photos__swiper">
-                    <swiper class="swiper gallery-top" :options="swiperOptionTop" ref="swiperTop">
-                        <swiper-slide class="slide-1"></swiper-slide>
-                        <swiper-slide class="slide-2"></swiper-slide>
-                        <swiper-slide class="slide-3"></swiper-slide>
-                        <swiper-slide class="slide-4"></swiper-slide>
-                        <swiper-slide class="slide-5"></swiper-slide>
-                        <swiper-slide class="slide-6"></swiper-slide>
-                        <swiper-slide class="slide-7"></swiper-slide>
+                    <div id="images" style="display: none" v-viewer="viewerOptions">
+                        <img v-for="(photo, index) in photosArr" :src="photo" :key="index" alt="" />
+                    </div>
+                    <swiper class="swiper gallery-top" :options="swiperOptionTop" ref="swiperTop" @click="show">
+                        <swiper-slide
+                            v-for="(photo, index) in photosArr"
+                            :key="index"
+                            :style="{ 'background-image': 'url(' + photo + ')' }"
+                        ></swiper-slide>
                     </swiper>
                     <swiper class="swiper gallery-thumbs" :options="swiperOptionThumbs" ref="swiperThumbs">
-                        <swiper-slide class="slide-1"></swiper-slide>
-                        <swiper-slide class="slide-2"></swiper-slide>
-                        <swiper-slide class="slide-3"></swiper-slide>
-                        <swiper-slide class="slide-4"></swiper-slide>
-                        <swiper-slide class="slide-5"></swiper-slide>
-                        <swiper-slide class="slide-6"></swiper-slide>
-                        <swiper-slide class="slide-7"></swiper-slide>
+                        <swiper-slide
+                            v-for="(photo, index) in photosArr"
+                            :key="index"
+                            :style="{ 'background-image': 'url(' + photo + ')' }"
+                        ></swiper-slide>
                     </swiper>
                 </div>
             </div>
@@ -33,11 +32,29 @@ import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 import 'swiper/css/swiper.css';
 import './styles.css';
 
+import 'viewerjs/dist/viewer.css';
+import { directive as viewer } from 'v-viewer';
+
+import photo_1 from '~/assets/jpg/photos/photo_1.jpg';
+import photo_2 from '~/assets/jpg/photos/photo_2.jpg';
+import photo_3 from '~/assets/jpg/photos/photo_3.jpg';
+import photo_4 from '~/assets/jpg/photos/photo_4.jpg';
+import photo_5 from '~/assets/jpg/photos/photo_5.jpg';
+import photo_6 from '~/assets/jpg/photos/photo_6.jpg';
+import photo_7 from '~/assets/jpg/photos/photo_7.jpg';
+
+const photosArr = [photo_1, photo_2, photo_3, photo_4, photo_5, photo_6, photo_7];
+
+const viewerOptions = { movable: false, button: true, navbar: true, title: false, keyboard: true };
+
 export default {
     name: 'Photos',
     components: {
         Swiper,
         SwiperSlide,
+    },
+    directives: {
+        viewer: viewer(),
     },
     data() {
         return {
@@ -56,12 +73,25 @@ export default {
                 touchRatio: 0.2,
                 slideToClickedSlide: true,
             },
+            photosArr,
+            activeSlide: 0,
+            viewerOptions,
         };
+    },
+    methods: {
+        show() {
+            const viewer = this.$el.querySelector('#images').$viewer;
+            viewer.view(this.activeSlide);
+            viewer.show();
+        },
     },
     mounted() {
         this.$nextTick(() => {
             const swiperTop = this.$refs.swiperTop.$swiper;
             const swiperThumbs = this.$refs.swiperThumbs.$swiper;
+            swiperTop.on('slideChange', () => {
+                this.activeSlide = swiperTop.activeIndex - 7;
+            });
             swiperTop.controller.control = swiperThumbs;
             swiperThumbs.controller.control = swiperTop;
         });
