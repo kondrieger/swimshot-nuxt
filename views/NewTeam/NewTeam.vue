4164c1877850bbcf130ec27b099d52d30f061c71
Nikita Semenov <kondrieger@gmail.com>
2021-12-06 20:30:28 +0300

    reworked new team block in process

--- views/NewTeam/NewTeam.vue
+++ views/NewTeam/NewTeam.vue
@@ -1,68 +1,189 @@
 <template>
-    <div id="new-team">
-        <div class="container new-team">
-            <h2 class="media__title" data-aos="fade-right" data-aos-once="true">
-                Наша <span class="blue">команда</span>
-            </h2>
-
-            <div class="new-team__wrap">
-                <div class="new-team__pic-list">
-                    <div class="new-team__pic-list-item js-tab" @click="showText(1)" id="tab-1">
-                        <Team class="new-team__pic-list-item-icon" />
-                    </div>
-                    <div class="new-team__pic-list-item js-tab" @click="showText(2)" id="tab-2">
-                        <img :src="Olympics" alt="" class="new-team__pic-list-item-img" />
-                    </div>
-                    <div class="new-team__pic-list-item js-tab" @click="showText(3)" id="tab-3">
-                        <YinYang class="new-team__pic-list-item-icon" />
-                    </div>
-                </div>
-                <div class="new-team__content">
-                    <p class="new-team__content-item js-text" id="text-1">
-                        У нас в команде: <br />3 основателя, 1 наставник, 6 тренеров, 2 менеджера, 1 администратор, 1
-                        Instagram-менеджер, 1 менеджер контекстной рекламы, 1 разработчик, 1 финансит-бухгалтер и 1
-                        юрист.
+    <div class="container new-team" id="team">
+        <h2 data-aos="fade-right" data-aos-once="true">Наши <span class="blue">тренеры</span></h2>
+        <div class="new-team__wrap">
+            <div class="new-team__info">
+                <div class="new-team__item-text">
+                    <p class="new-team__item-name">
+                        {{ activeTrainer.name }}
                     </p>
-                    <p class="new-team__content-item js-text" id="text-2">
-                        Среди них: <br />участник олимпийских игр, мастера спорта по обычному плаванию, по плаванию на
-                        открытой воде, кандидаты в мастера спорта по плаванию в ластах, люди с высшим образованием и
-                        дополнительной сертификацией.
-                    </p>
-                    <p class="new-team__content-item js-text" id="text-3">
-                        Спринтеры и стайеры. Совы и жаворонки. Левши и правши. Мы все такие разные, но мы все хотим,
-                        чтобы ты полюбил плавание. В Swim Shot ты точно найдешь того, кто поможет тебе кайфовать в воде.
-                    </p>
+                    <ul class="new-team__item-desc">
+                        <li v-for="(item, index) in activeTrainer.desc" :key="index">
+                            {{ item }}
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <VButton class="new-team__btn" text="ПОЗНАКОМЬСЯ САМ С СУПЕРГЕРОЯМИ SWIM SHOT!" />
+            <swiper data-aos="fade-up" data-aos-once="true" ref="mySwiper" class="" :options="swiperOptions">
+                <swiper-slide v-for="(man, index) in teamArr" :key="index">
+                    <div class="new-team__item">
+                        <img :src="man.photo" class="new-team__item-img" alt="Swim shot тренеры" />
+                        <a :href="`https://www.instagram.com/${man.inst}/`" target="_blank">
+                            <div class="new-team__item-social">
+                                <Instagram class="header__links-social-item-img" />
+                            </div>
+                        </a>
+                    </div>
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+            </swiper>
         </div>
     </div>
 </template>
 
 <script>
-import VButton from '~/components/VButton/VButton.vue';
-import Team from '~/assets/svg/team.svg';
-import Olympics from '~/assets/olympics.png';
-import YinYang from '~/assets/svg/yinYang.svg';
-import $ from 'jquery';
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
 import './styles.css';
 
-export default {
-    components: { VButton, Team, YinYang },
-    data() {
-        return { Olympics };
-    },
-    methods: {
-        showText(id) {
-            const elem = $(`#text-${id}, #tab-${id}`);
+import Instagram from '~/assets/svg/instagram.svg';
 
-            if (elem.hasClass('active')) {
-                elem.removeClass('active');
-            } else {
-                $('.js-text, .js-tab').removeClass('active');
-                elem.addClass('active');
-            }
-        },
+import davydov from '~/assets/jpg/new-team/davydov.jpg';
+import astapov from '~/assets/jpg/new-team/astapov.jpg';
+import kasandra from '~/assets/jpg/new-team/kasandra.jpg';
+import druzhinin from '~/assets/jpg/new-team/druzhinin.jpg';
+import georgieva from '~/assets/jpg/new-team/georgieva.jpg';
+import vanushechkin from '~/assets/jpg/team/vanushechkin.jpg';
+import timofeeva from '~/assets/jpg/new-team/timofeeva.jpg';
+import matveeva from '~/assets/jpg/new-team/matveeva.jpg';
+import grishin from '~/assets/jpg/new-team/grishin.jpg';
+
+const teamArr = [
+    {
+        photo: davydov,
+        name: 'Александр Давыдов',
+        desc: [
+            'Мастер спорта РФ по плаванию',
+            'Призер первенства России по плаванию на открытой воде',
+            'Пятикратный рекордсмен Зеленограда',
+            'Сооснователь школы плавания Swim Shot',
+            'Опыт тренерской деятельности четыре года',
+        ],
+        inst: 'davydov_alex98',
+    },
+    {
+        photo: astapov,
+        name: 'Александр Астапов',
+        desc: [
+            'Призер первенства Европы, чемпионатов и первенств России по плаванию на открытой воде',
+            'Мастер спорта России по плаванию',
+            'Сооснователь и старший тренер школы плавания Swim Shot',
+            'Автор методики обучения школы плавания Swim Shot',
+            'Опыт работы с детьми и взрослыми четыре года',
+        ],
+        inst: 'a_astapov',
+    },
+    {
+        photo: kasandra,
+        name: 'Сергей Касандра',
+        desc: [
+            'Кандидат в мастера спорта России по плаванию',
+            'Призер и победитель чемпионатов Зеленограда',
+            'Сертифицированный тренер по плаванию',
+            'Сооснователь школы плавания Swim Shot',
+            'Опыт работы тренером семь лет',
+        ],
+        inst: 'bacford',
+    },
+    {
+        photo: druzhinin,
+        name: 'Илья Дружинин',
+        desc: [
+            'Участник двух олимпийских игр Рио-2016 и Токио-2020',
+            'Многократный чемпион России на 800 и 1500 вольным стилем',
+            'Мастер спорта международного класса',
+            'Управляющий московского филиала школы плавания Swim Shot',
+            'Высшее образование по направлению физическая культура',
+        ],
+        inst: 'druzhban_',
+    },
+    {
+        photo: georgieva,
+        name: 'Александра Георгиева',
+        desc: [
+            'Кандидат в мастера спорта РФ',
+            'Призер первенства России',
+            'Победитель и призер Кубка СНГ',
+            'Опыт тренерской деятельности два года',
+        ],
+        inst: '_alexandrina__',
+    },
+    // {
+    //     photo: vanushechkin,
+    //     name: 'Сергей Ванюшечкин',
+    //     desc: [
+    //         'Образование инструктора по плаванию детей',
+    //         'Победитель чемпионата Москвы по акватлону',
+    //         'Первый взрослый разряд по плаванию и подводному плаванию',
+    //         'Сертифицированный специалист на курсах, связанных с тренерской деятельностью',
+    //         'Опыт работы тренером более шести лет',
+    //     ],
+    //     inst: 'sergey_vanyushechkin',
+    // },
+    {
+        photo: timofeeva,
+        name: 'Дарья Тимофеева',
+        desc: [
+            'Мастер спорта России по плаванию',
+            'Многократный призер чемпионатов и первенств Московской области',
+            'Участница всероссийских соревнований',
+            'Тренерский стаж один год',
+        ],
+        inst: '_daxaaa',
+    },
+    {
+        photo: matveeva,
+        name: 'Екатерина Матвеева',
+        desc: [
+            'Высшее образование по профилю физическая культура',
+            'Экс-учительница физкультуры',
+            'Опыт тренерской деятельности один год',
+        ],
+        inst: 'katunia_matveeva',
+    },
+    {
+        photo: grishin,
+        name: 'Егор Гришин',
+        desc: [
+            'Кандидат в мастера спорта по плаванию',
+            'Многократный победитель и призер чемпионатов и кубков калужской области',
+            'Тренерский стаж один год',
+        ],
+        inst: 'e.grishin_',
+    },
+];
+
+export default {
+    name: 'Team',
+    components: {
+        Swiper,
+        SwiperSlide,
+        Instagram,
+    },
+    directives: {
+        swiper: directive,
+    },
+    data() {
+        return {
+            teamArr,
+            swiperOptions: {
+                spaceBetween: 30,
+                slideToClickedSlide: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            },
+            activeTrainer: {},
+        };
+    },
+    mounted() {
+        this.$nextTick(() => {
+            const mySwiper = this.$refs.mySwiper.$swiper;
+            mySwiper.on('slideChange', () => {
+                this.activeTrainer = teamArr[mySwiper.activeIndex];
+            });
+        });
     },
 };
 </script>
