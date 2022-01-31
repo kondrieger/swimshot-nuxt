e987c6c82801f52e7ff10d6510f5343ebebd1c02
Nikita Semenov <kondrieger@gmail.com>
2022-01-31 14:18:06 +0300

    mango script edit

--- static/js/mango.js
+++ static/js/mango.js
@@ -3,5 +3,8 @@
     w[o] = w[o] || function() { (w[o].q = w[o].q || []).push(arguments) }; w[o].u = u; w[o].t = 1 * new Date();
     s = d.createElement('script'); s.async = 1; s.id = i; s.src = u;
     p = d.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
-}(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
-mgo({calltracking: {id: 26947, elements: [{"numberText":"74994305595"}]}});
+    }(window, document, '//widgets.mango-office.ru/widgets/mango.js', 'mango-js', 'mgo'));
+    mgo({calltracking: {id: 26947, elements: [{"numberText":"74994305595"}], onReady: function(event) {
+    document.querySelector('[href="tel:+74994305595"]').href = "tel:+"+event.number;
+    }
+    }});
